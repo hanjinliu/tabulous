228b1cdf3d0baef4f725d402e5ef69afdd08139f
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-08-27 09:56:56 +0900

    add test for spreadsheet

--- tests/test_spreadsheet.py
+++ tests/test_spreadsheet.py
@@ -53,3 +53,41 @@ def test_remove():
     assert_data_equal(qtable._data_raw, [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]])
     qtable.redo()
     assert_data_equal(qtable._data_raw, [["1", "2", "3"], ["7", "8", "9"]])
+
+def test_setting_cell_out_of_bound():
+    viewer = TableViewer(show=False)
+    sheet = viewer.add_spreadsheet()
+    qtable = cast(QSpreadSheet, viewer.tables[0].native)
+
+    qtable.setDataFrameValue(2, 1, "x")
+    assert sheet.data.shape == (3, 2)
+    qtable.setDataFrameValue(2, 3, "y")
+    assert sheet.data.shape == (3, 4)
+    qtable.setDataFrameValue(1, 1, "z")
+    assert sheet.data.shape == (3, 4)
+
+    qtable.undo()
+    assert sheet.data.shape == (3, 4)
+    qtable.undo()
+    assert sheet.data.shape == (3, 2)
+    qtable.undo()
+    assert sheet.data.shape == (0, 0)
+
+def test_setting_header_out_of_bound():
+    viewer = TableViewer(show=False)
+    sheet = viewer.add_spreadsheet()
+    qtable = cast(QSpreadSheet, viewer.tables[0].native)
+
+    qtable.setHorizontalHeaderValue(1, "col1")
+    assert sheet.data.shape == (0, 2)
+    qtable.setHorizontalHeaderValue(2, "col2")
+    assert sheet.data.shape == (0, 3)
+    qtable.setVerticalHeaderValue(2, "row2")
+    assert sheet.data.shape == (3, 3)
+
+    qtable.undo()
+    assert sheet.data.shape == (0, 3)
+    qtable.undo()
+    assert sheet.data.shape == (0, 2)
+    qtable.undo()
+    assert sheet.data.shape == (0, 0)
