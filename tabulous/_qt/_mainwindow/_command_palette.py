eba8a9a3ac081c1d47382fef2e7c7483cfcca844
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-12-05 23:04:57 +0900

    update command palette

--- tabulous/_qt/_mainwindow/_command_palette.py
+++ tabulous/_qt/_mainwindow/_command_palette.py
@@ -8,11 +8,52 @@ if TYPE_CHECKING:
 
 palette = get_palette("tabulous")
 
+default_group = palette.add_group("")
 file_group = palette.add_group("File")
 table_group = palette.add_group("Table")
 analysis_group = palette.add_group("Analysis")
 view_group = palette.add_group("View")
 plot_group = palette.add_group("Plot")
+selection_group = palette.add_group("Selection")
+
+
+@default_group.register("Show key map")
+def show_keymap(self: _QtMainWidgetBase):
+    self.showKeyMap()
+
+
+@default_group.register("Close window")
+def close_window(self: _QtMainWidgetBase):
+    self.close()
+
+
+@default_group.register("New window")
+def new_window(self: _QtMainWidgetBase):
+    viewer = self._table_viewer.__class__()
+    return viewer._qwidget.activateWindow()
+
+
+@default_group.register("Toggle toolbar visibility")
+def toggle_toolbar(self: _QtMainWidgetBase):
+    self.toggleToolBarVisibility()
+
+
+@default_group.register("Toggle fullscreen")
+def toggle_fullscreen(self: _QtMainWidgetBase):
+    if self.isFullScreen():
+        self.showNormal()
+    else:
+        self.showFullScreen()
+
+
+@default_group.register("Undo")
+def undo(self: _QtMainWidgetBase):
+    self._table_viewer.current_table.undo_manager.undo()
+
+
+@default_group.register("Redo")
+def redo(self: _QtMainWidgetBase):
+    self._table_viewer.current_table.undo_manager.redo()
 
 
 @file_group.register("Open a file as a table")
@@ -35,12 +76,12 @@ def open_sample(self: _QtMainWidgetBase):
     self._toolbar.open_sample()
 
 
-@table_group.register("Copy as table")
+@table_group.register("Copy current table as a new Table")
 def copy_as_table(self: _QtMainWidgetBase):
     self._toolbar.copy_as_table()
 
 
-@table_group.register("Copy as spreadsheet")
+@table_group.register("Copy current table as a new Spreadsheet")
 def copy_as_spreadsheet(self: _QtMainWidgetBase):
     self._toolbar.copy_as_spreadsheet()
 
@@ -183,3 +224,86 @@ def boxenplot(self: _QtMainWidgetBase):
 @plot_group.register("Create a new figure canvas")
 def new_figure(self: _QtMainWidgetBase):
     self._toolbar.new_figure()
+
+
+@selection_group.register("Copy cells")
+def copy(self: _QtMainWidgetBase):
+    self._table_viewer.copy_data()
+
+
+@selection_group.register("Copy cells with headers")
+def copy_with_header(self: _QtMainWidgetBase):
+    self._table_viewer.copy_data(header=True)
+
+
+@selection_group.register("Copy as a new Table")
+def copy_as_new_table(self: _QtMainWidgetBase):
+    self._table_viewer.current_table._qwidget._copy_as_new_table("table")
+
+
+@selection_group.register("Copy as a new SpreadSheet")
+def copy_as_new_spreadsheet(self: _QtMainWidgetBase):
+    self._table_viewer.current_table._qwidget._copy_as_new_table("spreadsheet")
+
+
+@selection_group.register("Copy as tab separated text")
+def copy_as_tab(self: _QtMainWidgetBase):
+    self._table_viewer.current_table._qwidget.copyToClipboard(headers=False, sep="\t")
+
+
+@selection_group.register("Copy as tab separated text with headers")
+def copy_as_tab_with_header(self: _QtMainWidgetBase):
+    self._table_viewer.current_table._qwidget.copyToClipboard(headers=True, sep="\t")
+
+
+@selection_group.register("Copy as comma separated text")
+def copy_as_comma(self: _QtMainWidgetBase):
+    self._table_viewer.current_table._qwidget.copyToClipboard(headers=False, sep=",")
+
+
+@selection_group.register("Copy as comma separated text with headers")
+def copy_as_comma_with_header(self: _QtMainWidgetBase):
+    self._table_viewer.current_table._qwidget.copyToClipboard(headers=True, sep=",")
+
+
+@selection_group.register("Copy as literal")
+def copy_as_literal(self: _QtMainWidgetBase):
+    self._table_viewer.current_table._qwidget._copy_as_literal()
+
+
+@selection_group.register("Paste from comma separated text")
+def paste_from_comma(self: _QtMainWidgetBase):
+    self._table_viewer.current_table._qwidget.pasteFromClipBoard(sep=",")
+
+
+@selection_group.register("Paste from numpy-style text")
+def paste_from_numpy(self: _QtMainWidgetBase):
+    self._table_viewer.current_table._qwidget._paste_numpy_str()
+
+
+@selection_group.register("Add highlight")
+def add_highlight(self: _QtMainWidgetBase):
+    qwidget = self._table_viewer.current_table._qwidget
+    qwidget.setHighlights(qwidget.highlights() + qwidget.selections())
+
+
+@selection_group.register("Select all")
+def select_all(self: _QtMainWidgetBase):
+    self._table_viewer.current_table._qwidget._qtable_view.selectAll()
+
+
+@selection_group.register("Cut selected cells")
+def cut(self: _QtMainWidgetBase):
+    qwidget = self._table_viewer.current_table._qwidget
+    qwidget.copyToClipboard(headers=False)
+    return qwidget.deleteValues()
+
+
+@selection_group.register("Paste cells")
+def paste(self: _QtMainWidgetBase):
+    self._table_viewer.paste_data()
+
+
+@selection_group.register("Delete selected cells")
+def delete(self: _QtMainWidgetBase):
+    self._table_viewer.current_table._qwidget.deleteValues()
