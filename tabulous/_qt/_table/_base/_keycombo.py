7ed2b442322ea11ad7ba38729730c8c6786bed15
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-01-10 22:53:00 +0900

    more test, remove view_mode support for table-only use case

--- tabulous/_qt/_table/_base/_keycombo.py
+++ tabulous/_qt/_table/_base/_keycombo.py
@@ -82,15 +82,7 @@ QBaseTable._keymap.bind("Ctrl+A")(
 QMutableTable._keymap.bind("Delete")(_check_no_viewer(QMutableTable.deleteValues))
 QMutableTable._keymap.bind("Backspace")(_check_no_viewer(QMutableTable.deleteValues))
 
-QBaseTable._keymap.bind("Ctrl+K")(_check_no_viewer(lambda x: None))
-QBaseTable._keymap.bind("Ctrl+K, P")(_check_no_viewer(QBaseTable.setPopupView))
-QBaseTable._keymap.bind("Ctrl+K, V", orientation="vertical")(
-    _check_no_viewer(QBaseTable.setDualView)
-)
-QBaseTable._keymap.bind("Ctrl+K, H", orientation="horizontal")(
-    _check_no_viewer(QBaseTable.setDualView)
-)
-QBaseTable._keymap.bind("Ctrl+K, N")(_check_no_viewer(QBaseTable.resetViewMode))
+QBaseTable._keymap.bind("Ctrl+K")(_check_no_viewer(QBaseTable.setFocus))
 QBaseTable._keymap.bind("Ctrl+H")(_check_no_viewer(QBaseTable.undoStackView))
 QBaseTable._keymap.bind("Menu")(_check_no_viewer(QBaseTable.showContextMenuAtIndex))
 QBaseTable._keymap.bind("F6")(_check_no_viewer(QBaseTable.raiseSlotError))
