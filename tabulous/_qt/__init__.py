599fb6b9ded9d2de371255fa15f265a34bea416c
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-02-12 23:56:26 +0900

    bump version to 0.5.0.rc0

--- tabulous/_qt/__init__.py
+++ tabulous/_qt/__init__.py
@@ -2,3 +2,14 @@ from ._table import QTableLayer, QSpreadSheet, QTableGroupBy, QTableDisplay
 from ._table_stack import QTabbedTableStack
 from ._mainwindow import QMainWindow, QMainWidget
 from ._app import get_app
+
+__all__ = [
+    "QTableLayer",
+    "QSpreadSheet",
+    "QTableGroupBy",
+    "QTableDisplay",
+    "QTabbedTableStack",
+    "QMainWindow",
+    "QMainWidget",
+    "get_app",
+]
