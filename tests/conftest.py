0f9fd12405967af6c52082913514de27e1739dc8
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-05-14 20:18:27 +0900

    update test configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import pytest
-from tabulous._utils import init_config
-from tabulous import TableViewer
 
 @pytest.fixture
 def make_tabulous_viewer():
+    from tabulous._utils import init_config
+    from tabulous import TableViewer
     def func(show=False):
         return TableViewer(show=show)
     with init_config():
