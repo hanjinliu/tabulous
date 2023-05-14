d9dcde0e36132f7e5a5b0314fc9b83d313ce19ca
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-05-14 11:43:14 +0900

    add pyproject.toml and workflow

--- justfile
+++ justfile
@@ -1,12 +1,8 @@
+set shell := ["powershell.exe", "-c"]
+
 doc:
 	sphinx-apidoc -f -o ./rst/apidoc ./tabulous
 	sphinx-build -b html ./rst ./docs
-
-release:
-	python setup.py sdist
-	python setup.py bdist_wheel
-	twine upload --repository testpypi dist/*
-	twine upload --repository pypi dist/*
 
 images:
 	python ./image/generate_figs.py
@@ -23,3 +19,6 @@ installer:
 	--add-data ".\tabulous\_qt\_icons;.\tabulous\_qt\_icons"  \
 	--add-data ".\tabulous\style\_style.qss;.\tabulous\style"  \
 	--add-data ".\tabulous\style\defaults.json;.\tabulous\style"
+
+test:
+    hatch -v run test:run
--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
-from setuptools import setup, find_packages
+import sys
 
-TABULOUS = "tabulous"
+sys.stderr.write(
+    """
+    ===============================================================
+    tabulous does not support `python setup.py install`. Please use
 
-with open(f"{TABULOUS}/__init__.py", encoding="utf-8") as f:
-    line = next(iter(f))
-    VERSION = line.strip().split()[-1][1:-1]
+        $ python -m pip install .
 
-with open("README.md") as f:
-    README = f.read()
+    instead.
+    ===============================================================
+    """
+)
+sys.exit(1)
 
 setup(
-    name=TABULOUS,
-    version=VERSION,
+    name="tabulous",
     description="A table data viewer for Python",
-    long_description=README,
     long_description_content_type="text/markdown",
     author="Hanjin Liu",
     author_email="liuhanjin-sc@g.ecc.u-tokyo.ac.jp",
     license="BSD 3-Clause",
     download_url="https://github.com/hanjinliu/tabulous",
-    packages=find_packages(exclude=["docs", "examples", "rst", "tests", "tests.*"]),
-    package_data={
-        TABULOUS: ["**/*.pyi", "*.pyi", "**/*.svg", "**/*.png", "**/*.qss", "**/*.json"]
-    },
     include_package_data=True,
     install_requires=[
         "magicgui>=0.5.1",
@@ -48,9 +46,6 @@ setup(
         "pyqt5": ["pyqt5>=5.12.3"],
         "pyqt6": ["pyqt6>=6.3.1"],
         "scikit-learn": ["scikit-learn>=1.0"],
-    },
-    entry_points={
-        "console_scripts": [f"{TABULOUS}={TABULOUS}.__main__:main"],
     },
     python_requires=">=3.8",
 )
